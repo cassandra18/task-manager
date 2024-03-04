76eae00be7e9dca154ac28d2df10848f34a9825f
cassandra18 <cassandralelei013@gmail.com>
2024-03-04 22:45:19 +0300

    coded the db config file

---
+++ backend/config/db.ts
@@ -0,0 +1,12 @@
+const config = require('config');
+
+const environment = process.env.NODE_ENV || 'development';
+const serverPort = config.get(`${environment}.port`);
+const databaseConfig = config.get(`${environment}.database`);
+const jwtSecret = config.get(`${environment}.jwtSecret`);
+
+module.exports = {
+  serverPort,
+  databaseConfig,
+  jwtSecret
+};
